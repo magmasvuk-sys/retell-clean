1d1005dbc10ed06b6138fefc79ccc79cce3f6442
magmasvuk-sys <magmasvuk@gmail.com>
2026-04-25 22:25:06 +0100

    Create Home component for main page

---
+++ app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>MagMasV Workforce Platform</h1>
+      <p>Start your training journey below.</p>
+
+      <a href="/entry">
+        <button style={{ padding: "12px 20px" }}>
+          Start Simulation
+        </button>
+      </a>
+    </main>
+  );
+}
